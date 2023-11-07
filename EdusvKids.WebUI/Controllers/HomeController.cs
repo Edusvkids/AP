efa55e220692b1212d4fac768bbf13b17b51e663
XiomaraCamp <jc9971859@gmailcom>
2023-11-07 11:00:06 -0600

    Mas cambios

--- EdusvKids.WebUI/Controllers/HomeController.cs
+++ EdusvKids.WebUI/Controllers/HomeController.cs
@@ -53,6 +53,11 @@ namespace EdusvKids.WebUI.Controllers
             return View();
         }
 
+        public IActionResult Inicio()
+        {
+            return View();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
