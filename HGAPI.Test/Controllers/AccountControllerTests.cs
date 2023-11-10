8ebf6d2779fd69c89d5f7db940ad37f651ab20c2
Bagg 12 <bryanalbertoguevaraguerrero@gmail.com>
2023-11-10 12:33:00 -0600

    pruebas unitarias

---
+++ HGAPI.Test/Controllers/AccountControllerTests.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Moq;
+using EdusvKids.WebUI.Controllers; 
+using HGAPI.DTOs.UserPlayerDTOs;
+using System;
+using Microsoft.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+
+namespace HGAPI.Tests.Controllers
+{
+    [TestClass]
+    public class AccountControllerTests
+    {
+        private AccountController _controller;
+        private Mock<IHttpClientFactory> _httpClientFactoryMock;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _httpClientFactoryMock = new Mock<IHttpClientFactory>();
+            _controller = new AccountController(_httpClientFactoryMock.Object);
+        }
+
+        [TestMethod]
+        public void SignIn_Get_ReturnsView()
+        {
+            var expectedModel = new UserLoginInputDTO();
+
+            var result = _controller.SignIn() as ViewResult;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(expectedModel, result.Model);
+        }
+
+        [TestMethod]
+        public async Task SignIn_Post_ValidUser_ReturnsRedirectToAction()
+        {
+            var userLogin = new UserLoginInputDTO();
+            var responseMessage = new HttpResponseMessage(HttpStatusCode.OK);
+
+            _httpClientFactoryMock.Setup(factory => factory.CreateClient("API")).Returns(new HttpClient(new Mock<HttpMessageHandler>().Object));
+            _controller.ControllerContext = new ControllerContext();
+            _controller.ControllerContext.HttpContext = new DefaultHttpContext();
+
+            var result = await _controller.SignIn(userLogin) as RedirectToActionResult;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Index", result.ActionName);
+            Assert.AreEqual("Home", result.ControllerName);
+        }
+
+        [TestMethod]
+        public async Task SignIn_Post_InvalidUser_ReturnsView()
+        {
+            var userLogin = new UserLoginInputDTO();
+            var responseMessage = new HttpResponseMessage(HttpStatusCode.BadRequest);
+
+            _httpClientFactoryMock.Setup(factory => factory.CreateClient("API")).Returns(new HttpClient(new Mock<HttpMessageHandler>().Object));
+            _controller.ControllerContext = new ControllerContext();
+            _controller.ControllerContext.HttpContext = new DefaultHttpContext();
+
+            var result = await _controller.SignIn(userLogin) as ViewResult;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(userLogin, result.Model);
+        }
+        [TestMethod]
+        public void SignUp_Get_ReturnsView()
+        {
+            var expectedModel = new CreateUserPlayerDTO();
+
+            var result = _controller.SignUp() as ViewResult;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(expectedModel, result.Model);
+        }
+
+        [TestMethod]
+        public async Task SignUp_Post_ValidUser_ReturnsRedirectToAction()
+        {
+            var user = new CreateUserPlayerDTO();
+            var responseMessage = new HttpResponseMessage(HttpStatusCode.OK);
+
+            _httpClientFactoryMock.Setup(factory => factory.CreateClient("API")).Returns(new HttpClient(new Mock<HttpMessageHandler>().Object));
+            _controller.ControllerContext = new ControllerContext();
+            _controller.ControllerContext.HttpContext = new DefaultHttpContext();
+
+            var result = await _controller.SignUp(user) as RedirectToActionResult;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Index", result.ActionName);
+            Assert.AreEqual("Home", result.ControllerName);
+        }
+
+        [TestMethod]
+        public async Task SignUp_Post_InvalidUser_ReturnsView()
+        {
+            var user = new CreateUserPlayerDTO();
+            var responseMessage = new HttpResponseMessage(HttpStatusCode.BadRequest);
+
+            _httpClientFactoryMock.Setup(factory => factory.CreateClient("API")).Returns(new HttpClient(new Mock<HttpMessageHandler>().Object));
+            _controller.ControllerContext = new ControllerContext();
+            _controller.ControllerContext.HttpContext = new DefaultHttpContext();
+
+            var result = await _controller.SignUp(user) as ViewResult;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(user, result.Model);
+        }
+
+    }
+}
---
+++ HGAPI.Test/Controllers/HomeControllerTests.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EdusvKids.WebUI.Controllers;
+using EdusvKids.WebUI.Models;
+using System.Diagnostics;
+
+namespace EdusvKids.WebUI.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+        private HomeController _controller;
+        private ILogger<HomeController> _logger;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _logger = new LoggerFactory().CreateLogger<HomeController>();
+            _controller = new HomeController(_logger);
+        }
+
+        [TestMethod]
+        public void Index_ReturnsViewResult()
+        {
+            var result = _controller.Index() as ViewResult;
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Productos_ReturnsViewResult()
+        {
+            var result = _controller.Productos() as ViewResult;
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Monedas_ReturnsViewResult()
+        {
+            var result = _controller.Monedas() as ViewResult;
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Descargar_ReturnsViewResult()
+        {
+            var result = _controller.Descargar() as ViewResult;
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Pagar_ReturnsViewResult()
+        {
+            var result = _controller.Pagar() as ViewResult;
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Cuenta_ReturnsViewResult()
+        {
+            var result = _controller.Cuenta() as ViewResult;
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Crear_ReturnsViewResult()
+        {
+            var result = _controller.Crear() as ViewResult;
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Vida_ReturnsViewResult()
+        {
+            var result = _controller.Vida() as ViewResult;
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Inicio_ReturnsViewResult()
+        {
+            var result = _controller.Inicio() as ViewResult;
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Error_ReturnsViewResultWithModel()
+        {
+            var result = _controller.Error() as ViewResult;
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result.Model, typeof(ErrorViewModel));
+        }
+    }
+}
